38a91719d7dcba0d51edc215ea5e93df0bfb395a
fralaferla <156893925+fralaferla@users.noreply.github.com>
2024-06-15 03:03:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function clickedEvent(){
   const dropup=document.querySelector('.dropup-content');
-  //one hour to remove a dot. from 'show' fucking hell thank you vs code I'm grateful
   dropup.classList.toggle('show');
 }
 const button=document.querySelector('.dropbtn');
